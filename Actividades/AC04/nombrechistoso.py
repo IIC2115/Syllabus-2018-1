6dd8b317cb0d6e77412b37f2538108a3df9fc48b
Hernan4444 <hfvaldivieso@uc.cl>
2018-04-02 13:45:35 -0300

    AC04

---
+++ Actividades/AC04/nombrechistoso.py
@@ -0,0 +1,6 @@
+PERSONAS = [(0, 'Agustín', 'Castejón', 64371), (1, 'Alicia', 'Falcón', 68884), (2, 'Martin', 'Luna', 78276),
+            (3, 'Gregorio', 'Galán', 57175), (4, 'Mohamed', 'Ordóñez', 7981), (5, 'Pilar', 'Solera', 58695),
+            (6, 'Victoria', 'Pont', 21339), (7, 'Ana', 'Villa', 42113), (8, 'Raquel', 'Rivero', 19294),
+            (9, 'Clara', 'Plana', 34915), (10, 'Rosa', 'Rocha', 58798), (11, 'Javier', 'Viana', 56251),
+            (12, 'Angela', 'Quintanilla', 62270)]
+JEFATURA = [(6, 9), (6, 10), (9, 0), (0, 2), (0, 4), (2, 3), (2, 1), (10, 5), (10, 8), (5, 11), (5, 12), (8, 7)]
